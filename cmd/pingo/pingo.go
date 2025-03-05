b9228c523f74940156e66832fe63ccebaad27930
yorick <78358953+YSchilling@users.noreply.github.com>
2025-03-05 01:32:24 +0100

    basic version

---
+++ cmd/pingo/pingo.go
@@ -0,0 +1,151 @@
+package main
+
+import (
+	"math/rand"
+
+	rl "github.com/gen2brain/raylib-go/raylib"
+)
+
+const (
+	windowWidth         = 1280
+	windowHeight        = 720
+	targetFPS           = 60
+	batDistanceToWindow = 32
+	batWidth            = 16
+	batHeight           = 256
+	ballSize            = 16
+	ballSpeed           = 4
+)
+
+type Ball = struct {
+	pos       rl.Vector2
+	size      float32
+	direction rl.Vector2
+}
+
+func updateBallPosition(ball *Ball, player1 rl.Rectangle, player2 rl.Rectangle) {
+	newPos := rl.Vector2Add(ball.pos, ball.direction)
+
+	// check collisions with border
+	if newPos.X < ballSize {
+		newPos.X = ballSize
+		ball.direction.X *= -1
+	}
+
+	if newPos.X+ball.size > windowWidth {
+		newPos.X = windowWidth - ball.size
+		ball.direction.X *= -1
+	}
+
+	if newPos.Y < ballSize {
+		newPos.Y = ballSize
+		ball.direction.Y *= -1
+	}
+
+	if newPos.Y+ball.size > windowHeight {
+		newPos.Y = windowHeight - ball.size
+		ball.direction.Y *= -1
+	}
+
+	// check collision with players
+	updateBallPositionPlayerCollision(ball, player1, &newPos)
+	updateBallPositionPlayerCollision(ball, player2, &newPos)
+
+	ball.pos = newPos
+}
+
+func updateBallPositionPlayerCollision(ball *Ball, player rl.Rectangle, newPos *rl.Vector2) {
+	if rl.CheckCollisionCircleRec(ball.pos, ball.size, player) {
+		if ball.pos.X > player.X+player.Width {
+			newPos.X = player.X + player.Width + ballSize + 1
+			ball.direction.X *= -1
+		}
+		if ball.pos.X < player.X {
+			newPos.X = player.X - ballSize - 1
+			ball.direction.X *= -1
+		}
+		if ball.pos.Y > player.Y+player.Height {
+			newPos.Y = player.Y + player.Height + ballSize + 1
+			ball.direction.Y *= -1
+		}
+		if ball.pos.Y < player.Y {
+			newPos.Y = player.Y - ballSize - 1
+			ball.direction.Y *= -1
+		}
+	}
+}
+
+func updatePlayerPosition(player *rl.Rectangle, isPlayer2 bool) {
+	var keyUp int32 = rl.KeyW
+	var keyDown int32 = rl.KeyS
+	if isPlayer2 {
+		keyUp = rl.KeyUp
+		keyDown = rl.KeyDown
+	}
+
+	if rl.IsKeyDown(keyDown) {
+		player.Y += 8
+	}
+
+	if rl.IsKeyDown(keyUp) {
+		player.Y -= 8
+	}
+
+	if player.Y < 0 {
+		player.Y = 0
+	}
+
+	if player.Y+player.Height > windowHeight {
+		player.Y = windowHeight - player.Height
+	}
+}
+
+func main() {
+	// setup
+	rl.InitWindow(windowWidth, windowHeight, "Pingo")
+	defer rl.CloseWindow()
+	rl.SetTargetFPS(targetFPS)
+
+	// game objects
+	player1 := rl.Rectangle{
+		X:      batDistanceToWindow,
+		Y:      windowHeight/2 - batHeight/2,
+		Width:  batWidth,
+		Height: batHeight,
+	}
+
+	player2 := rl.Rectangle{
+		X:      windowWidth - batWidth/2 - batDistanceToWindow,
+		Y:      windowHeight/2 - batHeight/2,
+		Width:  batWidth,
+		Height: batHeight,
+	}
+
+	ball := Ball{
+		pos:  rl.NewVector2(windowWidth/2-ballSize/2, windowHeight/2-ballSize/2),
+		size: ballSize,
+		direction: rl.NewVector2(
+			rand.Float32()*(float32(rand.Intn(10))+1),
+			rand.Float32()*(float32(rand.Intn(10))+1),
+		),
+	}
+
+	// game loop
+	for !rl.WindowShouldClose() {
+
+		// update
+		updatePlayerPosition(&player1, false)
+		updatePlayerPosition(&player2, true)
+		updateBallPosition(&ball, player1, player2)
+
+		// drawing
+		rl.BeginDrawing()
+		rl.ClearBackground(rl.Black)
+
+		rl.DrawRectangleRec(player1, rl.White)
+		rl.DrawRectangleRec(player2, rl.White)
+		rl.DrawCircleV(ball.pos, ball.size, rl.White)
+
+		rl.EndDrawing()
+	}
+}
